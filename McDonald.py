c96217cf2c2e98b4b54a51f3848c73db2bfe1006
ko tainen <tainenko@gmail.com>
2019-08-21 22:42:53 +0800

    Update README.md aned McDonald.py

--- McDonald.py
+++ McDonald.py
@@ -1,24 +1,28 @@
+# -*- coding=utf-8 -*-
 import re
 import requests
+import hashlib
 from datetime import datetime, timedelta
+import csv
+import argparse
 
-class McDonald(object):
+
+class McDonald:
     """docstring for McDonald."""
 
     def __init__(self, token):
-        super(McDonald, self).__init__()
         self.json = {
-          "access_token": token,
-          "source_info": {
-            "app_version": "2.2.0",
-            "device_time": "2019/01/24 00:00:00",
-            "device_uuid": "device_uuid",
-            "model_id": "Pixel XL",
-            "os_version": "7.1.1",
-            "platform": "Android"
-          }
+            "access_token": token,
+            "source_info": {
+                "app_version": "2.2.0",
+                "device_time": "2019/01/24 00:00:00",
+                "device_uuid": "device_uuid",
+                "model_id": "Pixel XL",
+                "os_version": "7.1.1",
+                "platform": "Android"
+            }
         }
-        self.respones = ""
+        self.responce = ""
         self.coupons = []
         self.stickers = 0
         self.expire_stickers = 0
@@ -26,18 +30,18 @@ class McDonald(object):
     # Basic lottery function
     def Lottery(self):
         # Request to get a lottery
-        self.respones = requests.post('https://api1.mcddailyapp.com/lottery/get_item', json = self.json).text
+        self.responce = requests.post('https://api1.mcddailyapp.com/lottery/get_item', json=self.json).text
 
         # If you don't like to have the return value which lottery event , you can delete all the code below
 
         # Convert string to dictionary
-        """self.respones = eval(self.respones)
+        """self.responce = eval(self.responce)
 
         # Check the return value of lottery
-        if 'coupon' in self.respones['results']:
-            result = self.respones['results']['coupon']['object_info']['title']
+        if 'coupon' in self.responce['results']:
+            result = self.responce['results']['coupon']['object_info']['title']
         else:
-            result = self.respones['results']['sticker']['object_info']['title']
+            result = self.responce['results']['sticker']['object_info']['title']
 
         # Return the result of lottery
         return self.Re(result)"""
@@ -45,19 +49,19 @@ class McDonald(object):
     # Get the coupon list
     def Coupon_List(self):
         # Request to get the coupon list
-        self.respones = requests.post('https://api1.mcddailyapp.com/coupon/get_list', json = self.json).text
-        count = self.respones.count('coupon_id') # Count the number of coupons
-        self.respones = eval(self.respones)      # Convert string to dictionary
+        self.responce = requests.post('https://api1.mcddailyapp.com/coupon/get_list', json=self.json).text
+        count = self.responce.count('coupon_id')  # Count the number of coupons
+        self.responce = eval(self.responce)  # Convert string to dictionary
 
         # Every coupons are going to be checked the status is unused and not expired
         for value in range(count):
-            status = self.respones['results']['coupons'][value]['status']
-            redeem_end_datetime = self.respones['results']['coupons'][value]['object_info']['redeem_end_datetime']
+            status = self.responce['results']['coupons'][value]['status']
+            redeem_end_datetime = self.responce['results']['coupons'][value]['object_info']['redeem_end_datetime']
             redeem_end_datetime = datetime.strptime(redeem_end_datetime, '%Y/%m/%d %H:%M:%S')
 
             # Status code is 1 also redeem_end_datetime is not yet
             if status == 1 and redeem_end_datetime - datetime.now() > timedelta():
-                coupon = self.respones['results']['coupons'][value]['object_info']['title']
+                coupon = self.responce['results']['coupons'][value]['object_info']['title']
                 coupon = self.Re(coupon)
                 self.coupons.append(coupon + 'E:' + str((redeem_end_datetime - datetime.now()).days))
 
@@ -70,13 +74,13 @@ class McDonald(object):
         self.expire_stickers = 0
 
         # Request to get the sticker list
-        self.respones = requests.post('https://api1.mcddailyapp.com/sticker/get_list', json = self.json).text
-        self.stickers = self.respones.count('歡樂貼') # Count the number of stickers
-        self.respones = eval(self.respones)          # Convert string to dictionary
+        self.responce = requests.post('https://api1.mcddailyapp.com/sticker/get_list', json=self.json).text
+        self.stickers = self.responce.count('歡樂貼')  # Count the number of stickers
+        self.responce = eval(self.responce)  # Convert string to dictionary
 
         # Every stickers are going to be checked is expired
         for value in range(self.stickers):
-            expire_datetime = self.respones['results']['stickers'][value]['object_info']['expire_datetime']
+            expire_datetime = self.responce['results']['stickers'][value]['object_info']['expire_datetime']
             expire_datetime = datetime.strptime(expire_datetime, '%Y/%m/%d %H:%M:%S')
             if expire_datetime.month == datetime.now().month:
                 self.expire_stickers += 1
@@ -86,8 +90,8 @@ class McDonald(object):
 
     # Request to get a sticker lottery
     def Sticker_lottery(self):
-        self.Sticker_List() # Get the stickers imformation
-        sticker_ids = []    # Sticker ids list
+        self.Sticker_List()  # Get the stickers imformation
+        sticker_ids = []  # Sticker ids list
 
         # Print the results of stickers imformation
         print('----- Sticker Imformation -----')
@@ -106,17 +110,17 @@ class McDonald(object):
 
                 # Get the 6 sticker ids
                 for value in range(6):
-                    sticker_ids.append(self.respones['results']['stickers'][value]['sticker_id'])
+                    sticker_ids.append(self.responce['results']['stickers'][value]['sticker_id'])
 
                 # Update the json dictionary
                 self.json['sticker_ids'] = sticker_ids
 
                 # Get a sticker lottery
-                self.respones = requests.post('https://api1.mcddailyapp.com/sticker/redeem', json = self.json).text
-                self.respones = eval(self.respones) # Convert string to dictionary
+                self.responce = requests.post('https://api1.mcddailyapp.com/sticker/redeem', json=self.json).text
+                self.responce = eval(self.responce)  # Convert string to dictionary
 
                 # Print the results of coupon imformation
-                coupon = self.respones['results']['coupon']['object_info']['title']
+                coupon = self.responce['results']['coupon']['object_info']['title']
                 coupon = self.Re(coupon)
                 print('You win a coupon !\n')
                 print(coupon)
@@ -126,3 +130,163 @@ class McDonald(object):
         coupon = re.sub(r'鷄', '雞', coupon)
         coupon = re.sub(r'\(G.*\)|\(S.*\)|_.*|\(新.*', '', coupon)
         return coupon
+
+
+class Mask:
+    """docstring for Mask."""
+
+    def __init__(self, account, password):
+        self.paramString = account + password  # Just Username + Password
+        self.account = account  # Username
+        self.password = password  # Password
+        self.access_token = ""  # Token
+        self.str1 = datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M:%S')  # Device Time
+        self.str2 = '2.2.0'  # App Version
+        self.str3 = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')  # Call time
+        self.ModelId = 'MIX 3'  # Model ID
+        self.OsVersion = '9'  # Android OS Version
+        self.Platform = 'Android'  # Platform
+        self.DeviceUuid = 'device_uuid'  # Device Uuid
+        self.OrderNo = self.DeviceUuid + self.str3  # Order No
+        self.cardNo = 'cardNo'  # Card NO
+
+    def Login(self):
+        # Mask = MD5('Mc' + OrderNo + Platform + OsVersion + ModelId + DeviceUuid + str1 + str2 + paramString + 'Donalds')
+        data = 'Mc%s%s%s%s%s%s%s%sDonalds' % (
+            self.OrderNo,
+            self.Platform,
+            self.OsVersion,
+            self.ModelId,
+            self.DeviceUuid,
+            self.str1,
+            self.str2,
+            self.paramString
+        )
+        mask = hashlib.md5()
+        mask.update(data.encode('utf-8'))
+
+        # Form data
+        json = {
+            "account": self.account,
+            "password": self.password,
+            "OrderNo": self.OrderNo,
+            "mask": mask.hexdigest(),
+            "source_info": {
+                "app_version": self.str2,
+                "device_time": self.str1,
+                "device_uuid": self.DeviceUuid,
+                "model_id": self.ModelId,
+                "os_version": self.OsVersion,
+                "platform": self.Platform,
+            }
+        }
+
+        # Get the response
+        response = requests.post('https://api.mcddaily.com.tw/login_by_mobile', json=json).text
+
+        # Clean the garbage date
+        response = response.replace('null', '""')
+        response = response.replace('true', '"true"')
+        response = response.replace('false', '"false"')
+
+        # Convert the string to dictionary type
+        response = eval(response)
+
+        # Get the token
+        self.access_token = response['results']['member_info']['access_token']
+
+        # Return the dictionary type of response
+        return response
+
+    def CardIM(self):
+        # Mask = MD5('Mc' + OrderNo + access_token + cardNo + callTime + 'Donalds')
+        data = 'Mc%s%s%s%sDonalds' % (
+            self.OrderNo,
+            self.access_token,
+            self.cardNo,
+            self.str3,
+        )
+        mask = hashlib.md5()
+        mask.update(data.encode('utf-8'))
+
+        # From data
+        json = {
+            "OrderNo": self.OrderNo,
+            "access_token": self.access_token,
+            "callTime": self.str3,
+            "cardNo": self.cardNo,
+            "mask": mask.hexdigest(),
+        }
+
+        # Get the response
+        response = requests.post('https://api.mcddaily.com.tw/queryBonus', json=json).text
+
+        # Convert the string to dictionary type
+        response = eval(response)
+
+        # Return the dictionary type of response
+        return response
+
+
+# Login example
+def get_list():
+    # User login page
+    Username = input('Username : ')
+    Password = input('Password : ')
+
+    # Login and get the imformation
+    Account = Mask(Username, Password)
+    list = Account.Login()
+
+    # Print the results
+    print('')
+    print('Login status : ' + list['rm'])
+    print('Username     : ' + list['results']['member_info']['name']['last_name'] +
+          list['results']['member_info']['name']['first_name'])
+    print('Token        : ' + list['results']['member_info']['access_token'])
+
+
+# Card imformation example
+def get_card():
+    # Login and get the token
+    Account = Mask('Username', 'Password')
+    Account.Login()
+
+    # Get the card imformation
+    list = Account.CardIM()
+
+    # Print the results
+    print('')
+    print('Money  : ' + list['bonusList'][2]['bonusVO']['qunatity'])
+    print('Points : ' + list['bonusList'][1]['bonusVO']['qunatity'])
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=False, help='the directory of csv file')
+    parser.add_argument('--output', type=str, required=False, help='the output filepath')
+
+    args = parser.parse_args()
+    filepath = args.file or 'users.csv'
+    output = args.output or 'output.csv'
+
+    lines = []
+    with open(filepath, newline='') as csvfile:
+        rows = csv.reader(csvfile)
+        for row in rows:
+            user = Mask(row[0], row[1])
+            token = user.Login()['results']['member_info']['access_token']
+            Account = McDonald(token)
+            # Get lottery
+            Account.Lottery()
+            # Get the coupon list
+            list = Account.Coupon_List()
+            lines.append([row[0]] + list)
+            print(','.join([row[0]] + list))
+
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for line in lines:
+            writer.writerow(line)
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # McDonald-s-APP
+## How to use
+create a csv file named users.csv  
+write the account,password in line.
 
-I have no idea .
+execute  
+```bash
+python McDonald.py
+```
+or assign the csv filepath
+```bash
+python McDonald.py --file (your csv file path)
+```
